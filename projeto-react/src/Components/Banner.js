126978918364c503a553af06338ea88a166cd0da
thiagoc <thiagoc@debian>
2023-06-19 20:35:34 -0300

    Update React

--- projeto-react/src/Components/Banner.js
+++ projeto-react/src/Components/Banner.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Banner() {
     return (
         <section className="w-full">
-            <img src="/assets/images/banner.jpg" alt="Banner"/>
+            <img src="/assets/images/banner_new.jpg" alt="Banner"/>
         </section>
     );
 }
